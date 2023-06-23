aa5537b3519bbdf246ece82c6be3a4863594d3f9
Pedro Henrique <pedrosales2806@gmail.com>
2023-06-23 15:04:49 -0300

    Ajustes no código

--- src/br/ufrn/imd/controller/TelaCadastroController.java
+++ src/br/ufrn/imd/controller/TelaCadastroController.java
@@ -11,7 +11,6 @@ import javafx.scene.control.Alert;
 import javafx.scene.control.Alert.AlertType;
 import javafx.stage.Stage;
 import javafx.scene.control.Button;
-import javafx.scene.control.PasswordField;
 import javafx.scene.control.RadioButton;
 import javafx.scene.control.TextField;
 
