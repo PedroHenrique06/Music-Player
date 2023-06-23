77d0d55227b66a3999de14e187e890a417a7f2d9
Pedro Henrique <pedrosales2806@gmail.com>
2023-06-23 13:24:24 -0300

    Correção do arquivo que foi errado

--- src/br/ufrn/imd/model/Musica.java
+++ src/br/ufrn/imd/model/Musica.java
@@ -3,7 +3,6 @@ package br.ufrn.imd.model;
 import java.io.File;
 
 public class Musica {
-<<<<<<< HEAD
 	private String titulo;
 	private int tamanhoSegundos;
 	private Boolean tocando;
@@ -66,59 +65,3 @@ public class Musica {
 	}
 
 }
-=======
-private String titulo;
-private int tamanhoSegundos;
-private Boolean tocando;
-private Boolean selecionado;
-private String local;
-private File arquivo;
-public String getTitulo() {
-	return titulo;
-}
-public void setTitulo(String titulo) {
-	this.titulo = titulo;
-}
-public int getTamanhoSegundos() {
-	return tamanhoSegundos;
-}
-public void setTamanhoSegundos(int tamanhoSegundos) {
-	this.tamanhoSegundos = tamanhoSegundos;
-}
-public Boolean getTocando() {
-	return tocando;
-}
-public void setTocando(Boolean tocando) {
-	this.tocando = tocando;
-}
-public Boolean getSelecionado() {
-	return selecionado;
-}
-public void setSelecionado(Boolean selecionado) {
-	this.selecionado = selecionado;
-}
-public String getLocal() {
-	return local;
-}
-public void setLocal(String local) {
-	this.local = local;
-}
-
-
-
-public void pausarMusica() {
-	
-}
-
-public void tocarMusica() {
-	
-}
-public File getArquivo() {
-	return arquivo;
-}
-public void setArquivo(File arquivo) {
-	this.arquivo = arquivo;
-}
-
-}
->>>>>>> ac25cdc9be37c9733e0c8879bb60f60733182356
