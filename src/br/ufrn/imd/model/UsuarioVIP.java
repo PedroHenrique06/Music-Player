3e95873e519460a316c86ab3abcfba827a735d2c
Guilherme Hermes <guilherme.hermes.053@ufrn.edu.br>
2023-06-25 22:30:34 -0300

    Fazendo o javadoc e melhorando a modelagem das classes

--- src/br/ufrn/imd/model/UsuarioVIP.java
+++ src/br/ufrn/imd/model/UsuarioVIP.java
@@ -2,27 +2,45 @@ package br.ufrn.imd.model;
 
 import java.util.ArrayList;
 
+/**
+ * Classe que representa um usuário VIP, que estende a classe Usuario.
+ */
 public class UsuarioVIP extends Usuario {
 
-		
-	public UsuarioVIP(String username, String senha) {
-		super(username, senha);
-	}
-	
-	ArrayList<Playlist> listaPlaylists = new ArrayList<>();
-	
-	
-	
-	public ArrayList<Playlist> getListaPlaylists() {
-		return listaPlaylists;
-	}
-	
-	public void addPlaylist() {
-		
-	}
-	
-	public void excludePlaylist() {
-		
-	}
+    private ArrayList<Playlist> listaPlaylists;
 
+    /**
+     * Construtor da classe UsuarioVIP.
+     *
+     * @param username O nome de usuário do usuário VIP.
+     * @param senha    A senha do usuário VIP.
+     */
+    public UsuarioVIP(String username, String senha) {
+        super(username, senha);
+        listaPlaylists = new ArrayList<>();
+    }
+
+    /**
+     * Obtém a lista de playlists do usuário VIP.
+     *
+     * @return A lista de playlists do usuário VIP.
+     */
+    public ArrayList<Playlist> getListaPlaylists() {
+        return listaPlaylists;
+    }
+
+    /**
+     * Adiciona uma nova playlist à lista de playlists do usuário VIP.
+     */
+    public void addPlaylist(Playlist playlist) {
+        // Implemente a lógica para adicionar uma nova playlist
+    	listaPlaylists.add(playlist);
+    }
+
+    /**
+     * Exclui uma playlist da lista de playlists do usuário VIP.
+     */
+    public void excludePlaylist(Playlist playlist) {
+        listaPlaylists.remove(playlist);
+    }
 }
