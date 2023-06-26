ccf12ef00fba0ddfb19eb2e58ce4295863838fb8
Guilherme Hermes <guilherme.hermes.053@ufrn.edu.br>
2023-06-25 22:28:29 -0300

    Adicionando função de reset para o momento de logout

--- src/br/ufrn/imd/dao/MusicaDAO.java
+++ src/br/ufrn/imd/dao/MusicaDAO.java
@@ -4,14 +4,27 @@ import java.util.ArrayList;
 
 import br.ufrn.imd.model.Musica;
 
+/**
+ * Classe DAO (Data Access Object) para a manipulação de músicas.
+ * Responsável por gerenciar a lista de músicas e realizar operações de adição e remoção.
+ */
 public class MusicaDAO {
     private static MusicaDAO instance;
     private ArrayList<Musica> listaMusicas;
 
+    /**
+     * Construtor privado da classe MusicaDAO.
+     * Inicializa a lista de músicas.
+     */
     private MusicaDAO() {
         listaMusicas = new ArrayList<>();
     }
 
+    /**
+     * Obtém a instância única da classe MusicaDAO (implementação do padrão Singleton).
+     *
+     * @return A instância única da classe MusicaDAO.
+     */
     public static MusicaDAO getInstance() {
         if (instance == null) {
             instance = new MusicaDAO();
@@ -19,16 +32,37 @@ public class MusicaDAO {
         return instance;
     }
 
+    /**
+     * Adiciona uma música à lista de músicas.
+     *
+     * @param musica A música a ser adicionada.
+     */
     public void addMusica(Musica musica) {
         listaMusicas.add(musica);
     }
 
+    /**
+     * Remove uma música da lista de músicas.
+     *
+     * @param musica A música a ser removida.
+     */
     public void removeMusica(Musica musica) {
         listaMusicas.remove(musica);
     }
-
+    
+    /**
+     * Reseta a instância. 
+     */
+    public void resetDAO() {
+        instance = null;
+        // Limpar os dados ou realizar outras ações necessárias
+    }
+    /**
+     * Obtém a lista de músicas.
+     *
+     * @return A lista de músicas.
+     */
     public ArrayList<Musica> getListaMusicas() {
         return listaMusicas;
     }
 }
-
--- src/br/ufrn/imd/dao/PlaylistDAO.java
+++ src/br/ufrn/imd/dao/PlaylistDAO.java
@@ -4,14 +4,27 @@ import java.util.ArrayList;
 
 import br.ufrn.imd.model.Playlist;
 
+/**
+ * Classe DAO (Data Access Object) para a manipulação de playlists.
+ * Responsável por gerenciar a lista de playlists e realizar operações de adição e remoção.
+ */
 public class PlaylistDAO {
     private static PlaylistDAO instance;
     private ArrayList<Playlist> listaPlaylists;
 
+    /**
+     * Construtor privado da classe PlaylistDAO.
+     * Inicializa a lista de playlists.
+     */
     private PlaylistDAO() {
         listaPlaylists = new ArrayList<>();
     }
 
+    /**
+     * Obtém a instância única da classe PlaylistDAO (implementação do padrão Singleton).
+     *
+     * @return A instância única da classe PlaylistDAO.
+     */
     public static PlaylistDAO getInstance() {
         if (instance == null) {
             instance = new PlaylistDAO();
@@ -19,16 +32,37 @@ public class PlaylistDAO {
         return instance;
     }
 
+    /**
+     * Reseta a instância. 
+     */
+    public void resetDAO() {
+        instance = null;
+        // Limpar os dados ou realizar outras ações necessárias
+    }
+    /**
+     * Adiciona uma playlist à lista de playlists.
+     *
+     * @param playlist A playlist a ser adicionada.
+     */
     public void adicionarPlaylist(Playlist playlist) {
         listaPlaylists.add(playlist);
     }
 
+    /**
+     * Remove uma playlist da lista de playlists.
+     *
+     * @param playlist A playlist a ser removida.
+     */
     public void removerPlaylist(Playlist playlist) {
         listaPlaylists.remove(playlist);
     }
 
+    /**
+     * Obtém a lista de playlists.
+     *
+     * @return A lista de playlists.
+     */
     public ArrayList<Playlist> getListaPlaylists() {
         return listaPlaylists;
     }
 }
-
